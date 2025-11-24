e4725b31aadb182f7da78a9434c67e2cfe844c09
azimbakhtiyarovich <abdishukurovazim@gmail.com>
2025-11-24 17:55:54 +0500

    ...

--- P2PLoan.Core/DTO's/Auth/RegisterDto.cs
+++ P2PLoan.Core/DTO's/Auth/RegisterDto.cs
@@ -7,15 +7,7 @@ using System.Threading.Tasks;
 namespace P2PLoan.Core.DTO.Auth;
 public class RegisterDto 
 {
-<<<<<<< HEAD
-    public string? Name { get; set; }
-    public string? Email { get; set; }
-    public string? PhoneNumber { get; set; }
-    public string? Password { get; set; }
-=======
-    public string Name { get; set; }
     public string Email { get; set; }
     public string PhoneNumber { get; set; }
-    public string Password { get; set; }
->>>>>>> b143b1cd3d4ea7e5fbfeacd7bd5e5d052c587005
+    public string PasswordHash { get; set; }
 }
