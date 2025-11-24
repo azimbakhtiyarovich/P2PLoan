b59f8a5fa6666ce2b19067f73f26f68697aebb90
azimbakhtiyarovich <abdishukurovazim@gmail.com>
2025-11-24 17:59:34 +0500

    .

--- P2PLoan.Core/Enum/RepaymentFrequency.cs
+++ P2PLoan.Core/Enum/RepaymentFrequency.cs
@@ -1,24 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-<<<<<<< HEAD:P2PLoan.Core/Enum/RepaymentFrequency.cs
 namespace P2PLoan.Core.Enum;
 public enum RepaymentFrequency : short 
 { 
-    Weekly = 0, 
-    Monthly = 1 
-=======
-namespace P2PLoan.Core.DTO_s.Contract;
-public class ContractDto
-{
-    public Guid Id { get; set; }
-    public Guid LoanId { get; set; }
-    public Guid LenderId { get; set; }
-    public DateTime SignedDate { get; set; }
-    public DateTime SignedTime { get; set; }
-    public string ContractName { get; set; }
->>>>>>> b143b1cd3d4ea7e5fbfeacd7bd5e5d052c587005:P2PLoan.Core/DTO's/Contract/ContractDto.cs
+    Daily = 0,
+    Weekly = 1, 
+    Monthly = 2,
+    Yearly = 3 
 }
